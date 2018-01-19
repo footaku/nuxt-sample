1da76d893c5b49eb4f4c710515c7d258b2fd6dca
fu-taku <age.of.pan+github@gmail.com>
2018-01-19 17:39:19 +0900

    Todo sample using components

---
+++ components/todo-item.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div>
+      <input @keyup.enter="add"/>
+    </div>
+    <ul v-for="(item, index) in todos" v-bind:key="index">
+      <li>{{ item }}　<button @click="remove(index)">remove</button></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default{
+  computed: {
+    todos: function () {
+      return this.$store.state.todo.list
+    }
+  },
+  methods: {
+    add: function (e) {
+      this.$store.commit('todo/add', e.target.value)
+      e.target.value = ''
+    },
+    remove: function (index) {
+      this.$store.commit('todo/remove', index)
+    }
+  }
+}
+</script>
---
+++ pages/todo.vue
@@ -0,0 +1,23 @@
+<<template>
+  <div id="sample">
+    <section id="title">
+      <span>{{ title }}</span>
+    </section>
+    <todoItem />
+  </div>
+</template>
+
+<script>
+import todoItem from '~/components/todo-item.vue'
+
+export default{
+  data () {
+    return {
+      title: 'This is todo list sample'
+    }
+  },
+  components: {
+    todoItem
+  }
+}
+</script>
